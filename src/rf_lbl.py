fc42c49fbf39325663175a49dbcb368d524becab
Akifumi Abe <akiabe0831@gmail.com>
2020-11-11 07:33:31 +0900

    update

--- src/rf_lbl.py
+++ src/rf_lbl.py
@@ -7,10 +7,6 @@ def run(fold):
     df = pd.read_csv("../input/train_folds.csv")
 
     features = [x for x in df.columns if x not in ["id", "target", "kfold"]]
-    for feat in features:
-        lbl_enc = preprocessing.LabelEncoder()
-        lbl_enc.fit(df[feat].values)
-        lbl_enc.transform(df[feat].values)
 
     df_train = df[df.kfold != fold].reset_index(drop=True)
     df_valid = df[df.kfold == fold].reset_index(drop=True)
@@ -18,15 +14,27 @@ def run(fold):
     xtrain = df_train.loc[:, features].values
     xvalid = df_valid.loc[:, features].values
 
-    ytrain = df_train.target.values
-    yvalid = df_valid.target.values
+    lbl_enc = preprocessing.LabelEncoder()
+    ytrain = lbl_enc.fit_transform(df_train.target.values)
+    yvalid = lbl_enc.fit_transform(df_valid.target.values)
 
     rf = ensemble.RandomForestClassifier()
     rf.fit(xtrain, ytrain)
-    pred = rf.predict_proba(xvalid)
-    print(pred)
+    pred = rf.predict_proba(xvalid)[:, 1]
 
+    log_loss = metrics.log_loss(yvalid, pred)
+    print(f"fold={fold}, loss={log_loss}")
 
+    df_valid.loc[:, "rf_lbl_pred"] = pred
+
+    return df_valid[["id", "sentiment", "kfold", "rf_lbl_pred"]]
 
 if __name__ == "__main__":
-    run(5)
+    dfs = []
+    for j in range(5):
+        temp_df = run(j)
+        dfs.append(temp_df)
+
+    fin_valid_df = pd.concat(dfs)
+    print(fin_valid_df.shape)
+    fin_valid_df.to_csv("../model_preds/rf_lbl.csv", index=False)
